0865996c89e9ba8b1d2ff67bc880b528a7e2de4c
Trương Văn Tú <37862349+tumeotu@users.noreply.github.com>
2020-03-08 14:29:21 +0700

    PhanMemQuanLyQuanCaPhe

---
+++ Source/QuanLiQuanCaPhe/QuanLiQuanCaPhe/Models/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiQuanCaPhe.Models
+{
+	class DataProvider
+	{
+		private static DataProvider _ISCreated;
+		public static DataProvider ISCreated
+		{
+			get
+			{
+				if (_ISCreated == null)
+					_ISCreated = new DataProvider();
+				return _ISCreated;
+			}
+			set
+			{
+				_ISCreated = value;
+			}
+		}
+		public QUAN_LY_QUAN_CAPHEEntities1 DB { get; set; }
+		 private DataProvider()
+		{
+			DB = new QUAN_LY_QUAN_CAPHEEntities1();
+		}
+	}
+}
